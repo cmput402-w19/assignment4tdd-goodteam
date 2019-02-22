9c9472eab48d9240d20217152f18ccb653c8c162
Kristen Newbury <knewbury@ualberta.ca>
2019-02-22 10:54:25 -0700

    GREEN board init implemented, with some square placeholder

--- src/main/java/cs/ualberta/cmput402/boardgame/board/Board.java
+++ src/main/java/cs/ualberta/cmput402/boardgame/board/Board.java
@@ -2,36 +2,48 @@ package cs.ualberta.cmput402.boardgame.board;
 
 public class Board {
 
-    public enum Square { PLACEHOLDER };
-    public enum Player { PLACEHOLDER };
+    public enum Player { RED, BLUE };
+    //just for now, will refactor to be real object list 
+    public enum Square { OCCUPIED, EMPTY};
     public Square board[][];
+    private int size = 5;
+    private Player currentPlayer;
+    private Player winner;
     
     public Board(){
-
+	board = new Square[size][size];
+	initBoard();
     }
 
     public void initBoard(){
-       
+	currentPlayer = Player.RED;
+	winner = null;
+
+	for (int i = 0; i < size; i++){
+	    for(int j = 0; j < size; j++){
+		if (i == 0 || i == size-1){
+		    board[i][j] = Square.OCCUPIED;
+		}else{
+		    board[i][j] = Square.EMPTY;
+		}
+	    }
+	}
     }
     
     public int getSize(){
-	return 0;
+	return size;
     }
 
     public Player getCurrentPlayer(){
-	Player player = Player.PLACEHOLDER;
-	return player;
+	return currentPlayer;
     }
 
     public Player getWinner(){
-
-    Player player = Player.PLACEHOLDER;
-        return player;
+	return winner;
     }
 	
     public Square getSquareAtPos(int i, int j){
-	Square square = Square.PLACEHOLDER;
-	return square;
+	return board[i][j];
     }
     
 }
