073a8a2807e167743c001e71e1cfd54ff114d94c
Kristen Newbury <knewbury@ualberta.ca>
2019-02-22 15:04:40 -0700

    RED piece test thorough and template piece impl

---
+++ src/main/java/cs/ualberta/cmput402/boardgame/board/Piece.java
@@ -0,0 +1,20 @@
+package cs.ualberta.cmput402.boardgame.board;
+
+public class Piece{
+
+    private String team;
+    private boolean masterStatus;
+    
+    public Piece(String color, boolean isMaster){
+	
+    }
+
+    public boolean isMaster(){
+	return false;
+    }
+
+    public String getTeam(){
+	return "";
+    }
+    
+}
---
+++ src/test/java/PieceTest.java
@@ -0,0 +1,43 @@
+import cs.ualberta.cmput402.boardgame.board.Piece;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runners.model.TestClass;
+
+/**                                                                           
+ * Created by knewbury on 02-22-19                                             
+ * This one is a bit overboard I'll admit
+ */
+
+public class PieceTest {
+
+    public Piece piece;
+    
+    @Test
+    public void testBlueStudentPiece(){
+	piece = new Piece("BLUE", false);
+	assert(piece.isMaster() == false);
+	assert(piece.getTeam() == "BLUE");
+    }
+
+    @Test
+    public void testRedMasterPiece(){
+	piece = new Piece("RED", true);
+	assert(piece.isMaster() == true);
+	assert(piece.getTeam() == "RED");
+    }
+
+    @Test
+    public void testRedStudentPiece(){
+	piece = new Piece("RED", false);
+	assert(piece.isMaster() == false);
+	assert(piece.getTeam() == "RED");
+    }
+
+    @Test
+    public void testBlueMasterPiece(){
+	piece = new Piece("BLUE", true);
+	assert(piece.isMaster() == true);
+	assert(piece.getTeam() == "BLUE");
+    }
+    
+}
