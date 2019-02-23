15dc39eb0c6ca1dc6fe93e10c89d9618a0c8f88a
Braedy Kuzma <braedy@ualberta.ca>
2019-02-22 18:45:20 -0700

    Initial blank board renderering.

--- src/main/java/cs/ualberta/cmput402/boardgame/Renderer.java
+++ src/main/java/cs/ualberta/cmput402/boardgame/Renderer.java
@@ -3,6 +3,9 @@ package cs.ualberta.cmput402.boardgame;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import javax.swing.*;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
 
 
 // Initial ideas based on https://stackoverflow.com/a/21142687/2379240
@@ -10,6 +13,7 @@ public class Renderer {
 
     // GUI elements.
     private final JPanel gui = new JPanel(new BorderLayout(3, 3));
+    private JButton[][] squares;
 
     // Assets.
     private Image[][] pieces = new Image[2][2];
@@ -45,12 +49,85 @@ public class Renderer {
         }
     }
 
-    Renderer() {
-        initGUI();
+    Renderer(Dimension dim) {
+        initGUI(dim);
     }
 
-    private void initGUI() {
-        createPieceImages();
+    public JPanel getGUI() {
+        return gui;
+    }
+
+    /**
+     * Builds the GUI from swing components.
+     *
+     * @param dim The dimensions of the board (number of tiles).
+     */
+    private void initGUI(Dimension dim) {
+        // Create assets.
+        createTileGraphics();
+
+        // Set up main GUI.
+        gui.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+        // Start setting up UI.
+        JPanel board = new JPanel(new GridLayout(dim.width, dim.height)) {
+
+            /**
+             * Override the preferred size to return the largest it can, in
+             * a square shape.  Must (must, must) be added to a GridBagLayout
+             * as the only component (it uses the parent as a guide to size)
+             * with no GridBagConstaint (so it is centered).
+             */
+            @Override
+            public final Dimension getPreferredSize() {
+                Dimension d = super.getPreferredSize();
+                Dimension prefSize = null;
+                Component c = getParent();
+                if (c == null) {
+                    prefSize = d;
+                } else if (c.getWidth() > d.getWidth() && c.getHeight() > d.getHeight()) {
+                    prefSize = c.getSize();
+                } else {
+                    prefSize = d;
+                }
+                int w = (int) prefSize.getWidth();
+                int h = (int) prefSize.getHeight();
+                // the smaller of the two sizes
+                int s = (w>h ? h : w);
+                return new Dimension(s,s);
+            }
+        };
+
+        // Create a border
+        board.setBorder(new CompoundBorder(
+                new EmptyBorder(4,4,4,4),
+                new LineBorder(Color.BLACK, 3, true)
+        ));
+
+        // Build square buttons.
+        squares = new JButton[dim.height][dim.width];
+        Insets buttonInset = new Insets(0, 0, 0,0); // No insets.
+        for (int i = 0; i < dim.height; ++i) {
+            for (int j = 0; j < dim.width; ++j) {
+                JButton button = new JButton();
+
+                // Remove insets
+                button.setMargin(buttonInset);
+
+                // Set appearance.
+                ImageIcon icon = new ImageIcon(empty);
+                button.setIcon(icon);
+                button.setBackground(Color.WHITE);
+
+                // Save reference to the button and add to the layout.
+                squares[i][j] = button;
+                board.add(button);
+            }
+        }
+
+        JPanel boardConstraints = new JPanel(new GridBagLayout());
+        boardConstraints.add(board);
+        gui.add(boardConstraints);
     }
 
     public void drawBoard() {
