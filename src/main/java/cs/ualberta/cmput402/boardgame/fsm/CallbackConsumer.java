7d2f639ef94d819bd2066951b0e2de0fa58f6b2d
Braedy Kuzma <braedy@ualberta.ca>
2019-02-25 08:45:59 -0700

    GREEN Outline new callback to consume.

--- src/main/java/cs/ualberta/cmput402/boardgame/fsm/CallbackConsumer.java
+++ src/main/java/cs/ualberta/cmput402/boardgame/fsm/CallbackConsumer.java
@@ -8,4 +8,10 @@ public interface CallbackConsumer {
      * @param y The y position of the square that got clicked.
      */
     public void onSquareClicked(int x, int y);
+
+    /**
+     * Called when a move is clicked with information about which move was clicked.
+     * @param idx The index of the move that was clicked for the currently active player.
+     */
+    public void onMoveClicked(int idx);
 }
