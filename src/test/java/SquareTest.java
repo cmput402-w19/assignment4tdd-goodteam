266e61a226f7e6e367ee4f91c478e41269950b86
Kristen Newbury <knewbury@ualberta.ca>
2019-02-25 18:58:33 -0700

    REFACTOR improve square test coverage by adding for case where square belongs to no one

--- src/test/java/SquareTest.java
+++ src/test/java/SquareTest.java
@@ -24,6 +24,7 @@ public class SquareTest {
 
     @Test
     public void testIsShrine(){
+	assert(square.belongsTo() == null);
 	square.setShrine(Player.Team.RED);
 	assert(square.isShrine() == true);
     }
