e5be98a1f4b688cd339562437c4526d283138ab4
Braedy Kuzma <braedy@ualberta.ca>
2019-02-26 00:46:12 -0700

    Fix offset misunderstanding.

--- src/main/java/cs/ualberta/cmput402/boardgame/Move.java
+++ src/main/java/cs/ualberta/cmput402/boardgame/Move.java
@@ -5,19 +5,19 @@ import java.awt.image.BufferedImage;
 
 public enum Move {
     //formatted L -> R appearance of possible squares
-    DRAGON(new Offsets[] {new Offsets(-2,1), new Offsets(-1,-1), new Offsets(2,1),
-			  new Offsets(1,-1)}),
+    DRAGON(new Offsets[] {new Offsets(-2,-1), new Offsets(-1,1), new Offsets(2,-1),
+			  new Offsets(1,1)}),
     
-    GOOSE(new Offsets[] {new Offsets(-1,1), new Offsets(-1,0), new Offsets(1,0),
-			 new Offsets(1,-1)}),
+    GOOSE(new Offsets[] {new Offsets(-1,-1), new Offsets(-1,0), new Offsets(1,0),
+			 new Offsets(1,1)}),
 
-    ELEPHANT(new Offsets[] {new Offsets(-1,1), new Offsets(-1,0), new Offsets(1,0),
-			    new Offsets(1,1)}),
+    ELEPHANT(new Offsets[] {new Offsets(-1,-1), new Offsets(-1,0), new Offsets(1,0),
+			    new Offsets(1,-1)}),
 
-    MONKEY(new Offsets[] {new Offsets(-1,1), new Offsets(-1,-1), new Offsets(1,1),
-			  new Offsets(1,-1)}),
+    MONKEY(new Offsets[] {new Offsets(-1,-1), new Offsets(-1,1), new Offsets(1,-1),
+			  new Offsets(1,1)}),
 
-    TIGER(new Offsets[] {new Offsets(0, 2), new Offsets(0, -1)});
+    TIGER(new Offsets[] {new Offsets(0, -2), new Offsets(0, 1)});
     
     private boolean selected;
     private Offsets[] offsets;
